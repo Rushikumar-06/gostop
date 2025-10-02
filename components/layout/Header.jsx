7363930bb78597658011efeffe6eaab4248f66e7
Rushikumar-06 <chundururushikumar@gmail.com>
2025-10-02 14:53:55 +0530

    added links in header to original website

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
     <div className="bg-white shadow-md sticky top-0 z-50">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         <div className="flex items-center justify-between w-full md:w-auto space-x-8">
-          <div className="text-2xl font-bold text-yellow-500">goSTOPS</div>
+         <div><a href="https://gostops.com/stay/Bengaluru/bengalurusb-hostel?checkin=2025-10-29&checkout=2025-10-30 " target="_blank"><img src="https://gostops.s3.amazonaws.com/static/src/img/gostops-logo.png" /></a></div>
           <nav className="hidden  md:flex justify-between w-full space-x-6 text-sm font-medium text-gray-600">
-            <a href="#" className=" flex items-center">Destinations <ChevronDown size={16} className="ml-1" /></a>
-            <a href="#" className="">Workation</a>
-            <a href="#" className="">Coliving</a>
-            <a href="#" className="flex items-center">goSTOPS for Business <ChevronDown size={16} className="ml-1" /></a>
-            <a href="#" className=" flex items-center">Collaborate with Us <ChevronDown size={16} className="ml-1" /></a>
+            <a href="https://gostops.com/stay/Bengaluru/bengalurusb-hostel?checkin=2025-10-29&checkout=2025-10-30 " target="_blank" className=" flex items-center">Destinations <ChevronDown size={16} className="ml-1" /></a>
+            <a  href="https://gostops.com/stay/Bengaluru/bengalurusb-hostel?checkin=2025-10-29&checkout=2025-10-30 " target="_blank" className="">Workation</a>
+            <a  href="https://gostops.com/stay/Bengaluru/bengalurusb-hostel?checkin=2025-10-29&checkout=2025-10-30 " target="_blank" className="">Coliving</a>
+            <a  href="https://gostops.com/stay/Bengaluru/bengalurusb-hostel?checkin=2025-10-29&checkout=2025-10-30 " target="_blank" className="flex items-center">goSTOPS for Business <ChevronDown size={16} className="ml-1" /></a>
+            <a  href="https://gostops.com/stay/Bengaluru/bengalurusb-hostel?checkin=2025-10-29&checkout=2025-10-30 " target="_blank" className=" flex items-center">Collaborate with Us <ChevronDown size={16} className="ml-1" /></a>
           </nav>
         </div>
         <div className="hidden sm:flex items-center space-x-4">
